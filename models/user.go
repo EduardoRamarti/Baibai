16d42bd47a9f17da30e08459b124bafff2232c6f
mikoushi <eduardo.ramarti@gmail.com>
2026-06-25 19:38:57 -0600

    Feature about retrieve all products from db and adds doc over the code

--- models/user.go
+++ models/user.go
@@ -1,11 +1,26 @@
 package models
 
+// User representa la estructura de datos de un usuario en la aplicación.
+// Se utiliza para almacenar y gestionar la información de los usuarios registrados.
 type User struct {
-	ID        string `json:"id"`
+	// ID es el identificador único del usuario (UUID).
+	ID string `json:"id"`
+
+	// FirstName es el nombre del usuario.
 	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Password  string `json:"-"`
-	UserName  string `json:"username"`
-	UserType  string `json:"usertype"`
+
+	// LastName es el apellido del usuario.
+	LastName string `json:"lastName"`
+
+	// Email es la dirección de correo electrónico del usuario (debe ser único).
+	Email string `json:"email"`
+
+	// Password es la contraseña del usuario. Se excluye de la serialización JSON (`json:"-"`).
+	Password string `json:"-"`
+
+	// UserName es el nombre de usuario (handle/username único) del usuario.
+	UserName string `json:"username"`
+
+	// UserType define el tipo o rol del usuario (ej: admin, customer, etc).
+	UserType string `json:"usertype"`
 }
